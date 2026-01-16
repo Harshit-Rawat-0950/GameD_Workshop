1cb4cb70c9d9a3521e831e96a50dc9336700d1ea
Harshit-Rawat-0950 <harshitdep@gmail.com>
2026-01-16 17:57:35 +0530

    commit three

--- Platformer/Assets/Scripts/player_movement.cs
+++ Platformer/Assets/Scripts/player_movement.cs
@@ -5,21 +5,33 @@ public class player_movement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody2D rb;
     //private Animator anim;
-    private bool grounded=false;
+    private bool grounded = false;
+    bool colliding;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float slideSpeed;
+    [SerializeField] float JumpForce;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         //anim = GetComponent<Animator>();
     }
-
     // Update is called once per frame
     private void FixedUpdate()
     {
-        float moveInput = Input.GetAxis("Horizontal");// Changes value depending on how long and how hard you press down the right or left arrow
-        rb.linearVelocity = new Vector2(moveInput * 10f, rb.linearVelocity.y);
+        float moveInput;
+        moveInput = Input.GetAxis("Horizontal");// Changes value depending on how long and how hard you press down the right or left arrow
+        if (!colliding)
+        {
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        }
+        else if (colliding && moveInput != 0 && !grounded)
+        {
+            Debug.Log(colliding);
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, -slideSpeed);
+        }
         if (Input.GetKey(KeyCode.UpArrow) && grounded)
         {
-            Jump(7f); // Jump with a force of 7
+            Jump(JumpForce); // Jump with a force of 7
         }
         if (moveInput > 0f)
         {
@@ -30,15 +42,21 @@ public class player_movement : MonoBehaviour
             transform.localScale = new Vector3(-10f, 8.5f, 1f);// Turn player to left
         }
     }
-    private void Jump(float jumpForce){
+    private void Jump(float jumpForce)
+    {
         rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        grounded =false;// When you jump,you need ot set grounded to false to prevent errors
+        grounded = false;// When you jump,you need ot set grounded to false to prevent errors
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            Debug.Log("IsGrounded");
             grounded = true;
+        }
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            colliding = true;
         }
     }
 }
